acafb3bf0ebad8e83fd9de31e86309d22e1f718f
ludia park <lypark1985@gmail.com>
2024-10-05 23:49:53 +0000

    AccountRepository content

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,24 @@
 package com.example.repository;
 
-public interface AccountRepository {
+import com.example.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    boolean existsByUsername(String username);
+    boolean existsByAccountId(Integer accountId);
+    Optional<Account> findByUsername(String username);
 }
+
+// you don't need create with JpaRepository
+// <Account, Integer>: 
+    // Account = type of entity this repo is managing
+    // Integer = type of primary key 
+// you don't need to write any concrete implementation of these methods, they are under the hood
+// all these method names are established methods of JpaRepository
+// Optional is a wrapper for nullable values, prevent NullPointerExceptions.
+    // we use it cuz we might not find a matching account with that username.
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,20 @@
 package com.example.service;
 
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class AccountService {
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    
 }
