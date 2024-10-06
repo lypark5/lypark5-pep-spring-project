e76a95c80c905a90aa3e604b2146671b2c1215f8
ludia park <lypark1985@gmail.com>
2024-10-06 05:45:40 +0000

    message repo content

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,4 +1,20 @@
 package com.example.repository;
 
-public interface MessageRepository {
+import com.example.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.List;
+
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+    List<Message> findAll();
+    Optional<Message> findById(Integer messageId);
+    List<Message> existsByPostedBy(Integer postedBy);
 }
+
+// "boolean existsById(Integer messsageId);" is unnecessary cuz we got Optional to handle null.
+// JpaRepository already includes .save() for create and update, no need to write it.
+// it also already includes .delete(), no need to write it.
